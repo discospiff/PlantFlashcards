02b21999de8e93cc8ef7e3179aac413a4664a000
jonesbr <brandan.jones@uc.edu>
2017-08-26 15:01:51 -0400

    Section 3

---
+++ app/src/main/java/com/plantflashcards/plantflashcards/FlashcardActivity.kt
@@ -0,0 +1,58 @@
+package com.plantflashcards.plantflashcards
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.plantflashcards.plantflashcards.dto.Plant
+
+class FlashcardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flashcard)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_flashcard, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun onButton4Clicked(v: View) {
+        val ONE_MINUTE = 60000;
+        var FIFTEEN_SECONDS = ONE_MINUTE/4;
+        Toast.makeText(this, "You clicked me!", Toast.LENGTH_LONG).show()
+
+        var foo:String? = "bar";
+        var length = foo?.length ?: 10
+        var redbud = Plant(1, "Cercis", "canadensis", "", "Eastern Redbud", 5)
+    }
+}
---
+++ app/src/main/java/com/plantflashcards/plantflashcards/dto/Plant.kt
@@ -0,0 +1,7 @@
+package com.plantflashcards.plantflashcards.dto
+
+/**
+ * Created by ucint on 7/24/2017.
+ */
+
+class Plant(var guid: Int, var genus : String, var species : String, var cultivar : String, var common : String, var height : Int = 0)
