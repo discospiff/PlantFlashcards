4b7c4a0a4ae86d8967f81f22cb8af96ea95891f5
jonesbr <brandan.jones@uc.edu>
2017-08-26 17:30:47 -0400

    4.1-4.4: Create objects from variable, new constructor, collections.

--- app/src/main/java/com/plantflashcards/plantflashcards/service/PlantService.kt
+++ app/src/main/java/com/plantflashcards/plantflashcards/service/PlantService.kt
@@ -11,14 +11,19 @@ class PlantService {
 
     fun parsePlantsFromJSONData(difficulty: String?) : List<Plant>? {
         // declare return type
-        var allPlants : List<Plant>? = null
+        var allPlants : ArrayList<Plant> = ArrayList<Plant>()
 
         var i = 1 + 1
         // open a network connection to our JSON data feed: http://plantplaces.com/perl/mobile/flashcard.pl
 
+        // create a new plant object
+        var plant = Plant()
+
         // parse the raw data into a set of Plant objects.
 
         // add the plant objects to a collection.
+        allPlants.add(plant)
+
 
         // return the collection.
         return allPlants
